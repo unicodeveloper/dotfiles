b4941c7a26f79139efd60d2fa42c8d30219f420d
Simon Owen Design <simon.owen.design@gmail.com>
2013-07-25 12:19:46 +0100

    Update path for z

--- install-deps.sh
+++ install-deps.sh
@@ -25,7 +25,7 @@
 # z, oh how i love you
 cd ~/
 git clone https://github.com/rupa/z.git
-chmod +x ~/z.sh
+chmod +x ~/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
 # z binary is already referenced from .bash_profile
