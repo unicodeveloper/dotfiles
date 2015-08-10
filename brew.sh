7beb207b3a99dcc1bebbaff4fe811658dc8e45f3
Simon Owen <s1000wen@gmail.com>
2015-08-10 17:54:17 +0100

    brew.sh: Install git-lfs

--- brew.sh
+++ brew.sh
@@ -82,6 +82,7 @@ brew install homebrew/php/php55 --with-gmp
 #brew install ack
 #brew install exiv2
 brew install git
+#brew install git-lfs
 #brew install imagemagick --with-webp
 brew install lua
 #brew install lynx
